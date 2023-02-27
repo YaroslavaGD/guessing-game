9d1a32bd98b285a180e193a6e4b5c8970fb77ada
Yaroslava Hryzadubova <yaroslava.gd@gmail.com>
2023-02-27 22:56:49 +0100

    feat: implement solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,23 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
+        this.mid = this.min + Math.round((this.max - this.min)/2);
     }
 
     guess() {
-
+        return this.mid;
     }
 
     lower() {
-
+        this.max = this.mid;
+        this.mid = this.min + Math.round((this.max - this.min)/2);
     }
 
     greater() {
-
+        this.min = this.mid;
+        this.mid = this.min + Math.round((this.max - this.min)/2);
     }
 }
 
